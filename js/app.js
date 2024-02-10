99547fb2712f1e09136135f6982f064a82bbc0a4
Gandhi Salgado <gandhi.salgado.ceron@gmail.com>
2024-02-10 15:41:51 -0600

    Ya funciona los botones de encriptar y desencriptar

--- js/app.js
+++ js/app.js
@@ -1,7 +1,80 @@
 function encriptar() {
-    console.log("encriptando...");
+    let i = 0; //para recorrer la cadena
+    let contText;
+
+    const texto = document.getElementById("textarea-HTML"); // tomamos text area
+    contText = texto.value;
+
+    if(contText === ""){
+        document.querySelector(".mensaje-inicio").style.display = "unset";
+        document.querySelector(".--ocultar").style.display = "none";
+        return
+    }
+    document.querySelector(".mensaje-inicio").style.display = "none";
+    while(i < contText.length){
+        if(contText[i] === "a"){
+            const derecha = contText.slice(i+1) //tomamos la parte derecha
+            const izquierda = contText.slice(0, i)
+            contText = izquierda + "ai" + derecha
+            i += 2;
+            continue;
+        }
+        if(contText[i] === "e"){
+            const derecha = contText.slice(i+1) //tomamos la parte derecha
+            const izquierda = contText.slice(0, i)
+            contText = izquierda + "enter" + derecha
+            i += 5;
+            continue;
+        }
+        if(contText[i] === "i"){
+            const derecha = contText.slice(i+1) //tomamos la parte derecha
+            const izquierda = contText.slice(0, i)
+            contText = izquierda + "imes" + derecha
+            i += 4;
+            continue;
+        }
+        if(contText[i] === "o"){
+            const derecha = contText.slice(i+1) //tomamos la parte derecha
+            const izquierda = contText.slice(0, i)
+            contText = izquierda + "ober" + derecha
+            i += 4;
+            continue;
+        }
+        if(contText[i] === "u"){
+            const derecha = contText.slice(i+1) //tomamos la parte derecha
+            const izquierda = contText.slice(0, i)
+            contText = izquierda + "ufat" + derecha
+            i += 4;
+            continue;
+        }
+        i++
+    }
+    document.querySelector(".--ocultar").style.display = "unset";
+    document.getElementById("textarea-codificado").innerHTML = contText;
+    
 }
 
 function desencriptar(){
-    console.log("desencriptar...");
+    let contText;
+
+    
+    const texto = document.getElementById("textarea-HTML"); // tomamos text area
+    contText = texto.value;
+    if(contText === ""){
+        document.querySelector(".mensaje-inicio").style.display = "unset";
+        document.querySelector(".--ocultar").style.display = "none";
+        return
+    }
+
+    document.querySelector(".mensaje-inicio").style.display = "none";
+
+    contText = contText.replaceAll("ai", "a");
+    contText = contText.replaceAll("enter", "e");
+    contText = contText.replaceAll("imes", "i");
+    contText = contText.replaceAll("ober", "o");
+    contText = contText.replaceAll("ufat", "u");
+
+    document.querySelector(".--ocultar").style.display = "unset";
+    document.getElementById("textarea-codificado").innerHTML = contText;
+
 }
